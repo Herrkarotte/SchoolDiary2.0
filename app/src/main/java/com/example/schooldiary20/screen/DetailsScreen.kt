eac183629f319494e94e4af17c821f450e39422b
HerrKarotte <dvp03ne@yandex.ru>
2025-05-11 19:07:51 +0300

    WIP: Add screen for schedules details

---
+++ app/src/main/java/com/example/schooldiary20/screen/DetailsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.schooldiary20.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailsScreen() {
+    Text("Какие-то детали по дню")
+}
