971e2195993de023eadd33e609f6ec520da6e933
HerrKarotte <dvp03ne@yandex.ru>
2025-05-17 23:16:38 +0300

    Add colors for grades

--- app/src/main/java/com/example/schooldiary20/ui/theme/Color.kt
+++ app/src/main/java/com/example/schooldiary20/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Grade5=Color(0xFF5F9841)
+val Grade4=Color(0xFF9DE675)
+val Grade3=Color(0xFFF3A342)
+val Grade2=Color(0xFFDF5252)
